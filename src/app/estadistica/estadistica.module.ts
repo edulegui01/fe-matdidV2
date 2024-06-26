eb8d48a443209905e454df5346b4cfa303dded13
eduardo <edulegui01@gmail.com>
2024-06-26 17:13:25 -0400

    creacion de la estructura para el modulo de estadisticas

---
+++ src/app/estadistica/estadistica.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavigationModule } from '../components/navigation.module';
+
+
+
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+
+   
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NavigationModule,
+    
+    
+    
+  ]
+})
+export class EstadisticaModule { }
